3c63b22cb8bdbef53b48a27957039ac010e3a167
lloydgabriel <lloydgabriel0619@gmail.com>
2026-06-02 14:07:52 +0800

    Fixed issues

--- Backend/app.py
+++ Backend/app.py
@@ -554,5 +554,5 @@ if __name__ == '__main__':
     else:
         print("❌ FAILED: Could not link to PostgreSQL. Check .env file.")
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
